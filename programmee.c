56a6083d1522f397e86fb8065629dab0a9942c29
12320311 <143862788+12320311@users.noreply.github.com>
2023-09-02 10:45:18 +0530

    Add files via upload

---
+++ programmee.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,i=0,sum=0;
+	printf("enter the value of n");
+	scanf("%d",&n);
+	while(i<=n)
+	{
+	sum=sum+i;
+	i++;
+}
+
+	printf("%d",sum);
+	return 0;
+}
